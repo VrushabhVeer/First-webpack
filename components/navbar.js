bb98d7d62d70b35c4d96fe4c8fbdcd8ae2af67f6
Vrushabh Veer <veervrushabh@gmail.com>
2022-04-30 11:42:35 +0530

    first webpack

---
+++ components/navbar.js
@@ -0,0 +1,17 @@
+function navbar() {
+    return `
+  <div class="left">
+    <p>Imag<span class="span">ine.</span></p>
+  </div>
+  <div class="option">
+    <p>Home</p>
+    <p>Features</p>
+    <p>About Us</p>
+    <p>Testimonials</p>
+    <p>Blog</p>
+    <p>Contact</p>
+  </div>`;
+}
+
+
+export { navbar };
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import {navbar} from "../components/navbar";
+import"../styles/navbar.css";
+
+let navbarDiv = document.getElementById("navbar");
+navbarDiv.innerHTML = navbar();
+
